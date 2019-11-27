0862b4e5f58568e68de6c725d8f91af297f4c174
Bartosz Świątek <shadzik@gmail.com>
2019-11-27 14:29:06 +0100

    Add create and update methods

--- apidaze/cdrhandlers/__init__.py
+++ apidaze/cdrhandlers/__init__.py
@@ -39,3 +39,68 @@ class Cdrhandlers(object):
         }
 
         return result
+
+    def create(self, url: str, name: str):
+        """
+            Creates a new CDR HTTP Handler. 
+            This will post the call detail (after a call) to the webhook URL you define.
+
+            Parameters
+            ----------
+            url: str
+                URL. of your application
+            name: str
+                Your App name
+
+            Returns
+            -------
+            dict
+                JSON response
+        """
+        response = self.http.request(
+            method=HttpMethodEnum.POST,
+            endpoint='/cdrhttphandlers',
+            payload={
+                'url': url,
+                'name': name
+            }
+            )
+
+        result = {
+            'body': response.json(),
+            'status_code': response.status_code
+        }
+
+        return result
+
+    def update(self, url: str, name: str):
+        """
+            Updates your current CDR HTTP Handler. 
+
+            Parameters
+            ----------
+            url: str
+                URL. of your application
+            name: str
+                Your App name
+
+            Returns
+            -------
+            dict
+                JSON response
+        """
+        response = self.http.request(
+            method=HttpMethodEnum.POST,
+            endpoint='/cdrhttphandlers',
+            payload={
+                'url': url,
+                'name': name
+            }
+            )
+
+        result = {
+            'body': response.json(),
+            'status_code': response.status_code
+        }
+
+        return result
