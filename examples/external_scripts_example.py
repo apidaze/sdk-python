54919326260ae123c35f9c690c5f9ecdff16ad9e
Bartosz Świątek <shadzik@gmail.com>
2020-03-10 16:13:41 +0100

    Remove unwanted create/remove methods from external_scripts module; unit tests and examples are gone as well

--- examples/external_scripts_example.py
+++ examples/external_scripts_example.py
@@ -14,11 +14,6 @@ def list_external_scripts():
     print(response)
 
 
-def create_external_script(name: str, url: str):
-    response = apidaze.external_scripts.create(name=name, url=url)
-    print(response)
-
-
 def get_external_script(id: int):
     response = apidaze.external_scripts.get(id)
     print(response)
@@ -29,13 +24,6 @@ def update_external_script(id: int, url: str, name: str):
     print(response)
 
 
-def remove_external_script(id: int):
-    response = apidaze.external_scripts.remove(id=id)
-    print(response)
-
-
 list_external_scripts()
 # get_external_script(1589)
-# create_external_script(name='test script blabla', url='http://url.url')
-# remove_external_script(1814)
 # update_external_script(1589, 'http://example.com/apidaze/a.xml', 'Bartoszs script')
